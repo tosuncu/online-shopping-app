03cb18c3263e75f74ef8ed91eeb9c70661734b2e
tosuncu <tosuncu07@gmail.com>
2021-01-21 00:24:59 +0300

    First commit

---
+++ src/main/java/com/tosuncu/shoppingapp/ShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.tosuncu.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppApplication.class, args);
+	}
+
+}
